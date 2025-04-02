4e83cb6e06e5c30cc7ae5cbbf76b9a40c895af80
Yegor Ivashchenko <9499507+y3g0r@users.noreply.github.com>
2025-04-02 22:01:53 +0100

    Init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { PostsList } from './components/PostsList'
+
+function App() {
+  return (
+    <>
+      <h1>Hello, post</h1>
+      <PostsList/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/PostsList.tsx
@@ -0,0 +1,8 @@
+export function PostsList() {
+  return (
+    <div>
+      <h1>Posts List</h1>
+      <p>This is the posts list component.</p>
+    </div>
+  );
+}
