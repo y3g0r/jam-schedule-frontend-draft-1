021180a383a3cbb4ddd9bf690cf3b7992d7a3bd8
Yegor Ivashchenko <9499507+y3g0r@users.noreply.github.com>
2025-05-03 20:27:44 +0100

    Address build failures

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { RootLayout } from './components/RootLayout.tsx'
 import { Blog } from './pages/Blog.tsx'
-import { Home } from './pages/Home.tsx'
 import { JamDetails } from './pages/JamDetails.tsx'
 import { Jams } from './pages/Jams.tsx'
 import { Schedule } from './pages/Schedule.tsx'
--- src/components/RootLayout.tsx
+++ src/components/RootLayout.tsx
@@ -1,5 +1,4 @@
 import { Outlet } from "react-router";
-import { NavBar } from "./NavBar";
 import { UserHeader } from "./UserHeader";
 
 export function RootLayout() {
