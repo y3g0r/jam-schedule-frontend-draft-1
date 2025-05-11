2e4c534cc26520c610fa122b6091b1e426ff0661
Yegor Ivashchenko <9499507+y3g0r@users.noreply.github.com>
2025-05-11 11:11:39 +0100

    Use generated sdk for GetJams

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { client } from './client/client.gen.ts'
 import { RootLayout } from './components/RootLayout.tsx'
 import { Blog } from './pages/Blog.tsx'
 import { JamDetails } from './pages/JamDetails.tsx'
@@ -5,6 +6,16 @@ import { Jams } from './pages/Jams.tsx'
 import { Schedule } from './pages/Schedule.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { createBrowserRouter, RouterProvider } from 'react-router'
+
+// configure internal service client
+client.setConfig({
+  // set default base url for requests
+  baseUrl: `${import.meta.env.VITE_BASE_URL}`,
+  // set default headers for requests
+  headers: {
+    Authorization: 'Bearer <token_from_service_client>',
+  },
+});
 
 const router = createBrowserRouter([
   {
--- src/components/jam/JamList.tsx
+++ src/components/jam/JamList.tsx
@@ -2,6 +2,8 @@ import { useQuery } from "@tanstack/react-query"
 import { getJams, JAM_LIST_QUERY_KEY, JamData } from "../../api/jams"
 import { JamListItem } from "./JamListItem"
 import { useAuth } from "@clerk/clerk-react"
+import { getJams as sdkGetJams } from "../../client/sdk.gen"
+import { Jam, Participant } from "../../client"
 
 
 export function JamList() {
@@ -9,7 +11,37 @@ export function JamList() {
 
     const {isLoading, isError, data, error} = useQuery({
         queryKey: [JAM_LIST_QUERY_KEY],
-        queryFn: () => getJams(getToken),
+        // queryFn: () => getJams(getToken),
+        queryFn: async () => {
+            const token = await getToken();
+
+            const {response, data, error} = await sdkGetJams({
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                }
+            })
+
+            if (error) {
+                console.log(error)
+                throw new Error(`Error fetching jams ${response.status} ${response.statusText}`)
+            }
+
+            // TODO: introduce return interface
+            return data!.map((backendJamData: Jam) => ({
+                    id: backendJamData.id,
+                    name: backendJamData.name,
+                    start: new Date(backendJamData.startTimestampSeconds * 1000),
+                    end: new Date(backendJamData.endTimestampSeconds * 1000),
+                    createdBy: backendJamData.createdBy,
+                    location: backendJamData.location,
+                    participants: backendJamData.participants.map((p: Participant) => ({
+                        email: p.email,
+                        response: p.response,
+                    }))
+                }))
+
+            return getJams(getToken)
+        },
         // refetchOnWindowFocus: false,  // Prevent unnecessary refetching on window focus
         // staleTime: 0, // Ensure data is refetched immediately after invalidation
         retry: false
